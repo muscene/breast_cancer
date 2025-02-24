3329055e7653a4623288c9d043b91ca294eeceb2
muscene <109787450+muscene@users.noreply.github.com>
2025-02-24 19:00:13 +0200

    Update app.py

--- app.py
+++ app.py
@@ -103,4 +103,4 @@ def upload_image():
 # def uploaded_file(filename):
 #     return send_from_directory(app.config["UPLOAD_FOLDER"], filename)
 if __name__ == '__main__':
-    app.run(debug=True, host='0.0.0.0', port=5000)
+    app.run(debug=False, host='0.0.0.0', port=5000)
